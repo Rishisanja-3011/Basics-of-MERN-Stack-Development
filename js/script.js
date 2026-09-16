eef88c3305c6835be213f46e7e2d940b99f209e4
Sanja Rishi <rishisanja99@gmail.com>
2026-09-16 19:07:41 +0530

    js intro

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// console.log("Hello There!")
+// console.warn("This is warning")
+// var a = 10
+
+// //alert("welcome home?")
+
+// var a = confirm("are you an adult ")
+
+// console.log(a)
+
+// var name = prompt("Enter your name...")
+// console.log(name)
+
+
+/* 
+JS DATATYPES
+ -premetive
+    -numbers
+    -string
+    -boolean
+    -undefined
+    -null
+    -symbol
+    -nan
+ -reference
+    -array
+    -object
+    -function
+*/
+
+
+var abc = function(){
+   console.log("Hey you called a funtion")
+}
+abc()
+
+
+var obj1 = {
+   user : 'Rishi',
+   surname : 'Sanja',
+   age : 21
+}
+
+console.log(obj1)
+console.log(obj1.user)
+
+//array of objects
+
+var arr = [{name:"Rishi",age:21}]
+
+console.log(arr[0].age)
+
+
+//Dom 
+
+// 4 pillars of dom i)
+var a = document.querySelector('#try1')
+a.innerHTML = "Hello World!"
+a.style.backgroundColor = 'red'
